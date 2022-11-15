b32f03984206e1a072540d3e908c9f20fa2b922a
monotera <nelson.mosquera@javeriana.edu.co>
2022-11-15 14:10:25 -0500

    Creates filter by name for characters

--- characters/views.py
+++ characters/views.py
@@ -4,8 +4,12 @@ from characters.serializer import CharacterSerializer
 from rest_framework import viewsets
 from .models import Character
 from rest_framework.response import Response
+from .character_filter import CharacterFilter
+from django_filters import rest_framework as filters
 # Create your views here.
 
 class CharacterViewSet(viewsets.ModelViewSet):
         queryset = Character.objects.all()
         serializer_class  = CharacterSerializer
+        filter_backends = (filters.DjangoFilterBackend,)
+        filterset_class  = CharacterFilter
